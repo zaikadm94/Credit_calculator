6d7ddca2e0df86ec34fb0ba791955d0815212665
zaikadm <zaikadm@dtechs.io>
2023-04-13 18:17:44 +0300

    fix percent

--- src/CreditPaymentService.java
+++ src/CreditPaymentService.java
@@ -1,9 +1,8 @@
 public class CreditPaymentService {
 
     public int payment(int amount_rub, int term_years) {
-
-        double percent = 9.99;
-        double monthlyPayment = (amount_rub * percent) / (1 - Math.pow(1 + percent, term_years * 12));
+        double percent = (9.99 / 100)/12;
+        double monthlyPayment = (amount_rub * percent) / (1 - Math.pow(1 + percent,-term_years * 12));
 
         return (int) monthlyPayment;
 
--- src/Main.java
+++ src/Main.java
@@ -1,7 +1,7 @@
 public class Main {
     public static void main(String[] args) {
         CreditPaymentService service = new CreditPaymentService();
-        int result = service.payment(1000000,1);
+        int result = service.payment(1000000,2);
         System.out.println(result);
 
     }
