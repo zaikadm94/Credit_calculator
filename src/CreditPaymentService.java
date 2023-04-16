70182d5b31d0f57bbc0931f92ce6b43309c16ecb
zaikadm <zaikadm@dtechs.io>
2023-04-16 17:07:20 +0300

    fix code

--- src/CreditPaymentService.java
+++ src/CreditPaymentService.java
@@ -1,11 +1,10 @@
 public class CreditPaymentService {
 
     public int payment(int amountRub, int termYears) {
-        double percent = (9.99 / 100)/12;
-        double monthlyPayment = (amountRub * percent) / (1 - Math.pow(1 + percent,-termYears * 12));
+        double percent = (9.99 / 100) / 12;
+        double monthlyPayment = (amountRub * percent) / (1 - Math.pow(1 + percent, -termYears * 12));
 
         return (int) monthlyPayment;
-
 
 
     }
